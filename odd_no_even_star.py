16fccfafba3f403d8dca8cddaf4475419f64ac4e
mohinisingh137 <mohinisingh20012007@gmail.com>
2025-08-27 11:44:42 +0530

    Add files via upload

---
+++ odd_no_even_star.py
@@ -0,0 +1,10 @@
+N = int(input("Enter the row: "))
+for i in range(1, N+1):
+    for j in range(1,i+1):
+        if j % 2 == 0:
+            print("*", end=" ")
+        else:
+            print(j,end="")
+    print()
+
+        
